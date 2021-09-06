e4ed421f62cf85a7c2f7fc097a1217512e951988
agentmike007 <manujasan23@gmail.com>
2021-09-06 07:51:31 +0530

    made a app.js file to talk to blockchain

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,20 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint256 public taskCoount = 0;
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("check out my linked in page");
+    }
+
+    function createTask(string memory _content) public {
+        taskCoount++;
+        tasks[taskCoount] = Task(taskCoount, _content, false);
+    }
 }
